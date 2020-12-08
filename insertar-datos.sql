fec6bceddb1ff53467f05f1b5ea846535710f561
fabianDev <72571855+fabianDeveloper@users.noreply.github.com>
2020-12-08 17:44:03 -0500

    Corrección insertar datos

--- insertar-datos.sql
+++ insertar-datos.sql
@@ -39,15 +39,63 @@ INSERT INTO CITA VALUES (3, '2020-11-14', '09:00:00');
 /* Datos: USUARIO                                               */
 /*==============================================================*/
 INSERT INTO USUARIO
-VALUES (1, 'Ian', 'Medina', '2020-11-14', '15:00:00', 1, 1, 1, 2, 1);
+VALUES (1, 'Ian', 'Medina', '2020-11-14', '15:00:00', 1, 1);
 INSERT INTO USUARIO
-VALUES (2, 'Mel', 'Santos', '2020-11-14', '14:00:00',1, 1, 1, 2, 1);
+VALUES (2, 'Mel', 'Santos', '2020-11-14', '14:00:00',1, 1);
 INSERT INTO USUARIO
-VALUES (3, 'Sebas', 'Proaño', '2020-11-14', '08:00:00',2, 2, 2, 1, 2);
+VALUES (3, 'Sebas', 'Proaño', '2020-11-14', '08:00:00',2, 2);
 INSERT INTO USUARIO
-VALUES (4, 'Adri', 'Gilces', '2020-11-14', '10:00:00',2, 2, 2, 1, 2);
+VALUES (4, 'Adri', 'Gilces', '2020-11-14', '10:00:00',2, 2);
 INSERT INTO USUARIO
-VALUES (5, 'Jhon', 'Vera', '2020-11-15', '18:00:00',1, 1, 2, 1, 1);
+VALUES (5, 'Jhon', 'Vera', '2020-11-15', '18:00:00',1, 1);
+
+/*==============================================================*/
+/* Datos: HOBBYUSUARIO                                          */
+/*==============================================================*/
+INSERT INTO HOBBYUSUARIO
+VALUES (1, 1, 1);
+INSERT INTO HOBBYUSUARIO
+VALUES (2, 2, 2);
+INSERT INTO HOBBYUSUARIO
+VALUES (3, 1, 3);
+INSERT INTO HOBBYUSUARIO
+VALUES (4, 2, 4);
+INSERT INTO HOBBYUSUARIO
+VALUES (5, 1, 5);
+INSERT INTO HOBBYUSUARIO
+VALUES (6, 2, 1);
+
+/*==============================================================*/
+/* Datos: DEPORTEUSUARIO                                        */
+/*==============================================================*/
+INSERT INTO DEPORTEUSUARIO
+VALUES (1, 1, 1);
+INSERT INTO DEPORTEUSUARIO
+VALUES (2, 2, 2);
+INSERT INTO DEPORTEUSUARIO
+VALUES (3, 1, 3);
+INSERT INTO DEPORTEUSUARIO
+VALUES (4, 2, 4);
+INSERT INTO DEPORTEUSUARIO
+VALUES (5, 1, 5);
+INSERT INTO DEPORTEUSUARIO
+VALUES (6, 2, 1);
+
+/*==============================================================*/
+/* Datos: MUSICAUSUARIO                                         */
+/*==============================================================*/
+INSERT INTO MUSICAUSUARIO
+VALUES (1, 1, 1);
+INSERT INTO MUSICAUSUARIO
+VALUES (2, 2, 2);
+INSERT INTO MUSICAUSUARIO
+VALUES (3, 1, 3);
+INSERT INTO MUSICAUSUARIO
+VALUES (4, 2, 4);
+INSERT INTO MUSICAUSUARIO
+VALUES (5, 1, 5);
+INSERT INTO MUSICAUSUARIO
+VALUES (6, 2, 1);
 
 /*==============================================================*/
 /* Datos: USUARIOCITA                                           */
